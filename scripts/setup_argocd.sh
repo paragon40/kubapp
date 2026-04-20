71b6dc948f47bcfa00d24e8bef5d70d4a0552d41
codest40 <generalcatmouse@gmail.com>
2026-04-20 10:42:31 +0100

    AUTO-COMMIT on 2026-04-20 10:42:31

--- scripts/setup_argocd.sh
+++
@@ -1,7 +0,0 @@
-
-kubectl -n argocd create secret generic repo-kubapp-ssh \
-  --from-file=sshPrivateKey=argocd-gitops
-
-argocd repo add git@github.com:paragon40/kubapp.git \
-  --ssh-private-key-path argocd-gitops
-
