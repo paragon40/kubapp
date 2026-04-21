d5c7755c78cc4103b3e76537819824af9e9b126b
codest40 <generalcatmouse@gmail.com>
2026-04-21 14:35:56 +0100

    chore: activate pipeline for dev

--- iac/infra/modules/eks/access.tf
+++ iac/infra/modules/eks/access.tf
@@ -23,7 +23,7 @@ resource "aws_eks_access_entry" "from_laptop" {
 
 resource "aws_eks_access_policy_association" "from_laptop" {
   cluster_name  = aws_eks_cluster.this.name
-  principal_arn = aws_eks_access_entry.from_laptop.admin_arn
+  principal_arn = aws_eks_access_entry.from_laptop.principal_arn
 
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
