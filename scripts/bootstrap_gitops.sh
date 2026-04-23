d5b7eb3a42d55e5950a7355cae3987d416117e24
codest40 <generalcatmouse@gmail.com>
2026-04-23 13:25:47 +0100

    AUTO-COMMIT on 2026-04-23 13:25:47

--- scripts/bootstrap_gitops.sh
+++ scripts/bootstrap_gitops.sh
@@ -6,10 +6,7 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 echo "GitOps Bootstrap Starting..."
 
 # Apply root application
-#kubectl apply -n argocd -f "$ROOT/gitops/argocd/nginx-app.yaml"
-kubectl apply -n argocd -f "$ROOT/gitops/argocd/apps/user-app.yaml"
-kubectl apply -n argocd -f "$ROOT/gitops/argocd/apps/admin-app.yaml"
-kubectl apply -n argocd -f "$ROOT/gitops/argocd/apps/ingress-app.yaml"
+kubectl apply  -n argocd -f "$ROOT/gitops/argocd/appset.yaml"
 
 echo "✅ Root app applied"
 echo "GitOps Bootstrap Complete"
