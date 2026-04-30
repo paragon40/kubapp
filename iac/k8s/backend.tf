03c4d2dae732e02f1c10cc1c689040b6cec5d189
codest40 <generalcatmouse@gmail.com>
2026-04-30 13:51:12 +0100

    [CHORE (Activate)]: run activation pipeline for dev - 2026-04-30 13:51:12

--- iac/k8s/backend.tf
+++ iac/k8s/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "kubapp-tf-state"
-    key            = "dev/k8s/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile  = true
-    encrypt        = true
+    bucket       = "kubapp-tf-state"
+    key          = "dev/k8s/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
