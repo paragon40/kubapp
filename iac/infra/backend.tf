2e2aef28bf73eee8a16f38ea18fd22d68211efb0
codest40 <generalcatmouse@gmail.com>
2026-04-30 13:10:47 +0100

    AUTO-COMMIT on 2026-04-30 13:10:47

--- iac/infra/backend.tf
+++ iac/infra/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket         = "kubapp-tf-state"
     key            = "dev/infra/terraform.tfstate"
     region         = "us-east-1"
+    use_lockfile = true
+    encrypt        = true
   }
 }
