78f622cc2a180dda79afe61dfdc7382b184fe636
codest40 <generalcatmouse@gmail.com>
2026-05-14 12:21:48 +0100

    LOCAL-COMMIT on 2026-05-14 12:21:48

--- iac/infra/modules/eks/other_nodes.tf
+++ iac/infra/modules/eks/other_nodes.tf
@@ -1,14 +1,14 @@
-resource "aws_eks_node_group" "ec2_nodes" {
+resource "aws_eks_node_group" "system_nodes" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${var.cluster_name}-ec2-nodes"
+  node_group_name = "${var.cluster_name}-system-nodes"
   node_role_arn   = var.node_role_arn
 
   subnet_ids = var.private_subnet_ids
 
   scaling_config {
-    desired_size = var.node_desired_capacity
-    max_size     = var.node_max_capacity
-    min_size     = var.node_min_capacity
+    desired_size = 2
+    max_size     = 4
+    min_size     = 2
   }
 
   instance_types = [var.node_instance_type]
@@ -17,33 +17,27 @@ resource "aws_eks_node_group" "ec2_nodes" {
   disk_size = 30
 
   labels = {
-    node_type = "ec2"
+    node_type = "system"
+    compute   = "system"
   }
 
-  taint {
-    key    = "compute"
-    value  = "ec2"
-    effect = "NO_SCHEDULE"
-  }
+  # IMPORTANT: NO TAINT
 
   update_config {
     max_unavailable = 1
   }
 
-  depends_on = [
-    aws_eks_cluster.this
-  ]
-
   tags = merge(var.tags, {
-    Name = "${var.cluster_name}-ec2-node"
+    Name = "${var.cluster_name}-system-node"
     resource-type = "eks-node-group"
-    eks-scope     = "node-group"
+    eks-scope     = "system-node-group"
     node-type     = "ec2"
-    node-group    = "primary"
+    node-group    = "secondary"
     node-role     = "worker"
-    workload     = "admin"
+    workload     = "system"
     eni-cluster = var.cluster_name
     eni-domain    = "compute"
     capacity-type = "on-demand"
   })
+
 }
