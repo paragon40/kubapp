2fd0cc6e94a7535818d30c91617eee2199d0bc72
codest40 <generalcatmouse@gmail.com>
2026-05-16 08:04:33 +0100

    [CHORE (Activate)]: run activation pipeline for dev - 2026-05-16 08:04:33

--- sys_monitor/infra/aws/start.sh
+++ sys_monitor/infra/aws/start.sh
@@ -163,6 +163,10 @@ done
 # ------------------------------------------------------------
 # Final Output
 # ------------------------------------------------------------
+echo "Generating test data..."
+export IP=$PUBLIC_IP
+bash "$ROOT_PROJECT/scripts/generate.sh"
+
 echo
 echo "============================================================"
 echo "DEPLOYMENT COMPLETE"
