35369afbc4e0693df88a68f6da013ef6bb9e7e44
codest40 <generalcatmouse@gmail.com>
2026-05-19 10:39:43 +0100

    [CHORE (Activate)]: run activation pipeline for dev - 2026-05-19 10:39:43

---
+++ sys_monitor/infra/aws/iam.tf
@@ -0,0 +1,84 @@
+
+resource "aws_iam_role" "ec2_role" {
+  name = "sys-monitor-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# ============================================================
+# CUSTOM POLICY FOR EKS ACCESS
+# ============================================================
+
+resource "aws_iam_role_policy" "eks_access" {
+  name = "sys-monitor-eks-access"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowEKSDescribe"
+        Effect = "Allow"
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListClusters",
+          "eks:AccessKubernetesApi"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowSTSIdentity"
+        Effect = "Allow"
+        Action = [
+          "sts:GetCallerIdentity"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+# INSTANCE PROFILE
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "sys-monitor-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+
+# Eks Access
+resource "aws_eks_access_entry" "sys_monitor" {
+  cluster_name  = "kubapp-dev"
+  principal_arn = aws_iam_role.ec2_role.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "sys_monitor_view" {
+  cluster_name  = "kubapp-dev"
+  principal_arn = aws_iam_role.ec2_role.arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
--- sys_monitor/infra/aws/main.tf
+++ sys_monitor/infra/aws/main.tf
@@ -146,6 +146,7 @@ resource "aws_instance" "sys_monitor" {
   vpc_security_group_ids      = [aws_security_group.sys_monitor.id]
   key_name                    = var.key_name
   associate_public_ip_address = false
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   root_block_device {
     volume_size           = 20
